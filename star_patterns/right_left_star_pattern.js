528333f939a1ba6df57576b8eccd74b01611796a
VladimirLQA <mr.kfpehtyrj@mail.ru>
2023-03-19 18:24:54 +0200

    added left triangle

---
+++ star_patterns/right_left_star_pattern.js
@@ -0,0 +1,19 @@
+function left_pattern(star, times){
+    let str = '';
+    for(let i = 1; i <= times; i++){
+        for(let j = 0; j < times - i; j++) {
+            str += ' ';
+        }
+        for (let k = 0; k < i; k++){
+            str += star;
+        }
+        str += `\n`;
+    }
+    return console.log(str);
+}
+
+left_pattern('*', 5);
+
+function right_pattern(star, times){
+
+}
