a7f8d246186628ab40c2aed4b633eae01b9586a3
VladimirLQA <mr.kfpehtyrj@mail.ru>
2023-03-19 18:42:33 +0200

    right_left_star_triangle_pattern.js

--- star_patterns/right_left_star_triangle_pattern.js
+++ star_patterns/right_left_star_triangle_pattern.js
@@ -15,5 +15,14 @@ function left_pattern(star, times){
 left_pattern('*', 5);
 
 function right_pattern(star, times){
-
+    let str = '';
+    for(let i = 1; i <= times; i++) {
+        for(let j = 0; j < i; j++) {
+            str += star;
+        }
+        str += '\n';
+    }
+    return console.log(str);
 }
+
+right_pattern('*', 5);
