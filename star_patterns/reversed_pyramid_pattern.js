4ad15581dce38a635264d0a5728ca871a3cf7dfe
VladimirLQA <mr.kfpehtyrj@mail.ru>
2023-03-24 18:08:12 +0200

    solution reversed_pyramid_pattern

---
+++ star_patterns/reversed_pyramid_pattern.js
@@ -0,0 +1,14 @@
+function reversed_pyramid_pattern(star, times) {
+    let str = '';
+    for(let i = 0; i < times; i++) {
+        for(let j = 0; j < i; j++) {
+            str += ' ';
+        }
+        for (let k = 1; k <= 2 * (times - i) - 1; k++) {
+            str += star;
+        }
+        str += '\n';
+    }
+    return console.log(str);
+}
+reversed_pyramid_pattern('*', 5)
