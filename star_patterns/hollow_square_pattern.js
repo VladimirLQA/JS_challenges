74d5b1631021be9e09ce79daa3fde69f789abde9
VladimirLQA <mr.kfpehtyrj@mail.ru>
2023-04-02 13:12:05 +0300

    little code reformat

--- star_patterns/hollow_square_pattern.js
+++ star_patterns/hollow_square_pattern.js
@@ -1,4 +1,4 @@
-/*
+/* * *
 
 *****
 *   *
@@ -6,17 +6,17 @@
 *   *
 *****
 
-* */
+* * */
 
 
 function hollow_square_pattern(star, times) { // `times` - rows/columns
     let str = '';
-    for(let i = 0; i < times; i++) {
-        for(let j = 0; j < times; j++){
-            if(i === 0 || i === times - 1) {
+    for (let i = 0; i < times; i++) {
+        for (let j = 0; j < times; j++) {
+            if (i === 0 || i === times - 1) {
                 str += star;
             } else {
-                if(j === 0 || j === times - 1) {
+                if (j === 0 || j === times - 1) {
                     str += star;
                 } else {
                     str += ' ';
