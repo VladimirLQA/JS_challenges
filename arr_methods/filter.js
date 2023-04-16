e1562a91a90715afdf44a9b6e88869dfe9ae8ef0
VladimirLQA <mr.kfpehtyrj@mail.ru>
2023-04-16 19:13:48 +0300

    own array methods implementation

---
+++ arr_methods/filter.js
@@ -0,0 +1,13 @@
+function ownFilter(array, filterFunction) {
+    let resultArr = [];
+    for ( let i = 0; i < array.length; i++) {
+        const result = filterFunction(array[i], i, array);
+        if(result) {
+            resultArr.push(array[i]);
+        }
+    }
+    return resultArr;
+}
+
+let testArray = [1, 2, 3, 4, 5];
+console.log(ownFilter(testArray, (el, i, arr) => el >= 3))
