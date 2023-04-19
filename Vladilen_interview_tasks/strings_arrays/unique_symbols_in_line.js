439f31b84d5851decfbabf4b20bb7ab9f32d4568
VladimirLQA <mr.kfpehtyrj@mail.ru>
2023-04-19 22:52:04 +0300

    vladilen_interview_tasks

---
+++ Vladilen_interview_tasks/strings_arrays/unique_symbols_in_line.js
@@ -0,0 +1,59 @@
+/*
+* Напишите функцию, которая определяет уникальны ли все символы в строке.
+* Регистр должен учитываться: `‘a’` и `‘A’` разные символы.
+* Input: String
+* Output: Boolean
+* */
+
+function isUnique(string) {
+    for (let i = 0; i < string.length; i++) {
+        const char = string[i];
+
+        if ( string.lastIndexOf(char) !== i) {
+            return false;
+        }
+    }
+
+    return true
+}
+
+console.log(isUnique('abcdef')) // -> true
+console.log(isUnique('1234567')) // -> true
+console.log(isUnique('abcABC')) // -> true
+console.log(isUnique('abcadef')) // -> false
+
+
+// second option ------------------
+
+function isUnique(string) {
+    const set = new Set();
+
+    for (let i = 0; i < string.length; i++) {
+        const char = string[i];
+
+        if (set.has(char)) {
+            return false
+        }
+        set.add(char);
+    }
+    return true
+}
+
+
+console.log(isUnique('abcdef')) // -> true
+console.log(isUnique('1234567')) // -> true
+console.log(isUnique('abcABC')) // -> true
+console.log(isUnique('abcadef')) // -> false
+
+
+// third option -----------------
+
+function isUnique(string) {
+    return new Set(string).size = string.length;
+}
+
+
+console.log(isUnique('abcdef')) // -> true
+console.log(isUnique('1234567')) // -> true
+console.log(isUnique('abcABC')) // -> true
+console.log(isUnique('abcadef')) // -> false
