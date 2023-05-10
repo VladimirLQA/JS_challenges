1b4941b3f6b0fa62d249d6200427fd2b3d6c9088
VladimirLQA <mr.kfpehtyrj@mail.ru>
2023-05-10 22:14:13 +0300

    description + reverse solution

---
+++ codewars/esreveR.js
@@ -0,0 +1,58 @@
+/*
+* Write a function reverse which reverses a list
+* (or in clojure's case, any list-like data structure)
+* (the dedicated builtin(s) functionalities are deactivated)
+*/
+
+let testArray1 = [1,2,3];
+let testArray2 = [1,null,14,"two"];
+
+{
+    function reverse(array) {
+        let result = [];
+        for (let i = array.length - 1; i >= 0; i--) {
+            result.push(array[i])
+        }
+        return result;
+    }
+    console.log(reverse(testArray1))
+    console.log(reverse(testArray2))
+}
+
+console.log(`========================================`)
+
+{
+    function reverse (array) {
+        return array.reduceRight((a, b) => { return a.push(b), a}, [])
+        // return array.reduceRight((a, b) => [...a, b], [])
+    }
+
+
+    console.log(reverse(testArray1))
+    console.log(reverse(testArray2))
+}
+
+console.log(`========================================`)
+
+{
+    function reverse(array) {
+        return array.reduce((a, b) => {
+            return [b].concat(a)
+        }, [])
+    }
+
+    console.log(reverse(testArray1))
+    console.log(reverse(testArray2))
+}
+
+console.log(`========================================`)
+
+{
+    function reverse(array) {
+        return array.map((v, i) =>  array[array.length - 1 - i]);
+    }
+
+    console.log(reverse(testArray1))
+    console.log(reverse(testArray2))
+}
+
