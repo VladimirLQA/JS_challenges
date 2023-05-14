495c6d6d79b8d9c611ce2be65e403740f66e1532
VladimirLQA <mr.kfpehtyrj@mail.ru>
2023-05-14 21:33:42 +0300

    ...

--- codewars/Square(n)_Sum.js
+++ codewars/Square(n)_Sum.js
@@ -4,11 +4,39 @@
 * For example, for [1, 2, 2] it should return 9
 */
 
-function squareSum(numbers){
-    if(numbers.length === 0) return 0;
-    return numbers.map(v => Math.pow(v, 2)).reduce((a, b) => a + b);
+{
+    function squareSum(numbers){
+        return numbers.map(v => Math.pow(v, 2)).reduce((sum, curr) => sum + curr, 0);
+    }
+    console.log(squareSum([1,2])); // 5
+    console.log(squareSum([0, 3, 4, 5])); // 50
+    console.log(squareSum([])); // 0
 }
 
-console.log(squareSum([1,2])); // 5
-console.log(squareSum([0, 3, 4, 5])); // 50
-console.log(squareSum([])); // 0
+console.log(`=================================================================`);
+
+{
+    function squareSum(numbers){
+
+        return numbers.reduce((sum, curr) => sum + (curr * curr), 0);
+    }
+    console.log(squareSum([1,2])); // 5
+    console.log(squareSum([0, 3, 4, 5])); // 50
+    console.log(squareSum([])); // 0
+}
+
+console.log(`=================================================================`);
+
+{
+    function squareSum(numbers){
+        let result = 0;
+        numbers.forEach((number) => {
+            result += number * number
+        });
+        return result;
+    }
+    console.log(squareSum([1,2])); // 5
+    console.log(squareSum([0, 3, 4, 5])); // 50
+    console.log(squareSum([])); // 0
+}
+
