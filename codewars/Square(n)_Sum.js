3b3fef136680f7cad32eeaef6c75c25a94fc09c8
VladimirLQA <mr.kfpehtyrj@mail.ru>
2023-05-14 21:26:16 +0300

    Square (n) Sum

---
+++ codewars/Square(n)_Sum.js
@@ -0,0 +1,14 @@
+/*
+* Complete the square sum function so that it squares each number
+* passed into it and then sums the results together.
+* For example, for [1, 2, 2] it should return 9
+*/
+
+function squareSum(numbers){
+    if(numbers.length === 0) return 0;
+    return numbers.map(v => Math.pow(v, 2)).reduce((a, b) => a + b);
+}
+
+console.log(squareSum([1,2])); // 5
+console.log(squareSum([0, 3, 4, 5])); // 50
+console.log(squareSum([])); // 0
