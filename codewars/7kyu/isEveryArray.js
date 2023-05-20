4e2e5e642c55779e8961b10d34944fb1e634a59d
VladimirLQA <mr.kfpehtyrj@mail.ru>
2023-05-20 20:02:55 +0300

    isEveryArray

---
+++ codewars/7kyu/isEveryArray.js
@@ -0,0 +1,48 @@
+/*
+* Is every value in the array an array?
+* [[1],[2]] => true
+* ['1','2'] => false
+* [{1:1},{2:2}] => false
+*/
+
+{
+    function isEveryArray(value) {
+        return value.reduce((acc, curr) => {
+            return Array.isArray(curr)
+        }, Array.isArray(value));
+    }
+
+    console.log(isEveryArray([]));
+    console.log(isEveryArray([['string']]));
+    console.log(isEveryArray([[], {}]));
+    console.log(isEveryArray(['A', 'R', 'R', 'A', 'Y']));
+    console.log(isEveryArray([[1], [2], [3]]));
+}
+
+console.log(`==============================================`);
+
+{
+    function isEveryArray(value) {
+        return value.every(el => Array.isArray(el));
+    }
+
+    console.log(isEveryArray([]));
+    console.log(isEveryArray([['string']]));
+    console.log(isEveryArray([[], {}]));
+    console.log(isEveryArray(['A', 'R', 'R', 'A', 'Y']));
+    console.log(isEveryArray([[1], [2], [3]]));
+}
+
+console.log(`==============================================`);
+
+{
+    function isEveryArray(value) {
+        return value.every(Array.isArray);
+    }
+
+    console.log(isEveryArray([]));
+    console.log(isEveryArray([['string']]));
+    console.log(isEveryArray([[], {}]));
+    console.log(isEveryArray(['A', 'R', 'R', 'A', 'Y']));
+    console.log(isEveryArray([[1], [2], [3]]));
+}
