7ecd7579a84307f389e1c086a2c3746e3acafd1b
VolodymyLQA <vovaomgxdlazurenko@gmail.com>
2023-05-31 12:41:15 +0300

    dfsdf

--- codewars/8kyu/Find_the_Difference_in_Age_between_Oldest_and_Youngest_Family_Members.js
+++ codewars/8kyu/Find_the_Difference_in_Age_between_Oldest_and_Youngest_Family_Members.js
@@ -43,4 +43,10 @@ console.log(`=================================================`);
     };
     console.log(differenceInAges([82, 15, 6, 38, 35])); // [6, 82, 76]
     console.log(differenceInAges([57, 99, 14, 32])); // [14, 99, 85]
+    ываыв
+
+    ываіваі
+
+    dfsdf
+    df
 }
