9682c1e8790273e5f13eb79e4db6b47420182f1b
VolodymyLQA <lazurenkov@edin.ua>
2023-05-31 09:17:12 +0300

    plus solution codewars

---
+++ codewars/8kyu/Find_the_Difference_in_Age_between_Oldest_and_Youngest_Family_Members.js
@@ -0,0 +1,46 @@
+/*
+* You will be given an array of all the family members' ages, in any order.
+* The ages will be given in whole numbers, so a baby of 5 months,
+* will have an ascribed ‘age’ of 0.
+* Return a new array (a tuple in Python) with [youngest age, oldest age,
+* difference between the youngest and oldest age].*/
+
+{
+    function differenceInAges(ages){
+        let sorted = ages.map(el => Math.floor(el)).sort((a, b) => a - b);
+        let minAge = sorted.shift();  // sorted[0]
+        let maxAge = sorted.pop();    // sorted[sorted.length - 1]
+        let differenceAge = maxAge - minAge;
+        return [minAge, maxAge, differenceAge];
+    }
+
+    console.log(differenceInAges([82, 15, 6, 38, 35])); // [6, 82, 76]
+    console.log(differenceInAges([57, 99, 14, 32])); // [14, 99, 85]
+}
+
+console.log(`=================================================`);
+
+{
+    function differenceInAges (ages) {
+        let max = Math.max(...ages),
+            min = Math.min(...ages),
+            diff = max - min
+
+        return [min, max, diff]
+    }
+
+    console.log(differenceInAges([82, 15, 6, 38, 35])); // [6, 82, 76]
+    console.log(differenceInAges([57, 99, 14, 32])); // [14, 99, 85]
+}
+
+console.log(`=================================================`);
+
+{
+    const differenceInAges = (ages) => {
+        let youngest = Math.min.apply(null, ages);
+        let oldest = Math.max.apply(null, ages);
+        return [youngest, oldest, oldest - youngest];
+    };
+    console.log(differenceInAges([82, 15, 6, 38, 35])); // [6, 82, 76]
+    console.log(differenceInAges([57, 99, 14, 32])); // [14, 99, 85]
+}
