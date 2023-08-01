b4546c1a835ea1c2e94abdaa0b1e699f0f6dc5ce
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-08-01 23:13:21 +0300

    + solution

---
+++ codewars/8kyu/Convert_number_to_reversed_array_of_digits.ts
@@ -0,0 +1,10 @@
+
+function digitize(n: number): number[] {
+    return [...String(n)].map(v => +v).reverse();
+}
+
+console.log(digitize(23582357));
+console.log(digitize(984764738));
+console.log(digitize(45762893920));
+console.log(digitize(548702838394));
+
