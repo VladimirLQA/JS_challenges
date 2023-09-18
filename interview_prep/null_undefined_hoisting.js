e75a48bc5d9a9590edf569b7383ded0a90ef1443
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-09-18 21:46:57 +0300

    renamed + nul_undefined_hoisting

---
+++ interview_prep/null_undefined_hoisting.js
@@ -0,0 +1,23 @@
+// null vs undefined
+let var1;
+console.log(var1);
+console.log(typeof  var1);
+
+let var2 = null;
+console.log(var2);
+console.log(typeof var2);
+
+// hoisting
+
+// Q 1
+// console.log(foo);
+// foo = 1;
+
+// Q 2
+// console.log(foo);
+// var foo = 2;
+
+// Q 3
+// foo = 3;
+// console.log(foo);
+// var foo;
