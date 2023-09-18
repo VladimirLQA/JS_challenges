ff0fbf96738b213bc068218648729ac9edbeb433
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-09-18 22:07:32 +0300

    closures

---
+++ interview_prep/closures.js
@@ -0,0 +1,34 @@
+
+// Create a counter function which has increment and getValue functionality
+
+function counter() {
+    let count = 0;
+
+    this.getValue = () => {
+        return count;
+    }
+
+    return {
+        increment: ( value = 1) => {
+            count += value;
+        },
+        getValue: () => {
+            return count;
+        },
+    }
+}
+
+let c = counter();
+
+console.log(c.getValue());
+c.increment(2);
+c.increment();
+console.log(c.getValue());
+
+const privateSecret = () => {
+    const secret = 'foo';
+    return () => secret;
+};
+
+const getSecret = privateSecret();
+console.log(getSecret());
