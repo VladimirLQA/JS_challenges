3f1709217afc305f914043ef2157b95f2b726161
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-09-24 22:25:15 +0300

    ...

--- codewars/7kyu/vowel_count.js
+++ codewars/7kyu/vowel_count.js
@@ -30,3 +30,11 @@
 
     console.log(getCount("abracadabra")); // 5
 }
+
+{
+    function getCount(str) {
+        return str.replace(/[^aeiou]/gi, "").length;
+    }
+
+    console.log(getCount("abracadabra")); // 5
+}
