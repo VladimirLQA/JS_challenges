1a237b54b83e7cb99cc157da55c4812e57f18f0f
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-10-22 14:11:08 +0300

    ..

---
+++ how_programming_works/SEF1/matrix.js
@@ -0,0 +1,19 @@
+// Use nested for loop to find max value in 2d matrix
+// For example max([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+// should return 9
+
+const twoDimensionalArr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+const max = (matrix) => {
+  let max = 0;
+  matrix.forEach((entity) => {
+    entity.forEach((value) => {
+      if (value > max) max = value;
+    });
+  });
+
+  return max;
+};
+
+
+console.log(max(twoDimensionalArr)); // 9
