11b7cbe13fc4d8b9380424569f6bc54a5045165e
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-11-05 22:02:44 +0200

    plus alternative solution

--- codewars/6-5kyu/Find-the-odd-int.js
+++ codewars/6-5kyu/Find-the-odd-int.js
@@ -1,16 +1,24 @@
-const findOdd = (n) => {
-  const hash = {};
-  let result = 0;
+{
+  const findOdd = (n) => {
+    const hash = {};
+    let result = 0;
 
-  n.forEach((el) => {
-    hash[el] === undefined ? hash[el] = 1 : hash[el]++;
+    n.forEach((el) => {
+      hash[el] === undefined ? hash[el] = 1 : hash[el]++;
+      for (const key in hash) {
+        if (hash[key] % 2 !== 0) result = +key;
+      }
+    });
+    return result;
+  };
 
-    for (const key in hash) {
-      if (hash[key] % 2 !== 0) result = el;
-    }
-
-  });
-  return result;
+  console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
 }
 
-console.log(findOdd([1, 1, 1, 1, 2, 2, 3, 3, 3]));
+console.log('<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>');
+
+{
+  const findOdd = (n) => n.reduce((a, b) => a ^ b);
+
+  console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
+}
