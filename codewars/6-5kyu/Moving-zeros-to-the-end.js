908167f2453cde0686d56de1ad295ddd97a49ba4
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-11-08 22:04:09 +0200

    moving zeros to the end 5 kyu

---
+++ codewars/6-5kyu/Moving-zeros-to-the-end.js
@@ -0,0 +1,57 @@
+{
+  // will reorder whole array even non zero value - not very suitable
+  const moveZeros = arr => {
+    return arr.sort((el1, el2) => el2 - el1);
+  };
+
+  console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
+}
+
+{
+  const moveZeros = arr => {
+    return arr.sort((el1, _) => {
+      if (el1 === 0) return 1;
+      else return -1;
+    });
+  };
+
+  console.log(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
+}
+
+{
+  const moveZeros = arr => {
+    return arr.sort((a, b) => {
+      if (a !== 0 && b === 0) return -1;
+      if (a === 0 && b !== 0) return 1;
+      return 0;
+    });
+  };
+
+  console.log(moveZeros([false, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
+}
+
+{
+  const isNotZero = element => element !== 0;
+  const isZero = element => element === 0;
+  const moveZeros = arr => arr.filter(isNotZero).concat(arr.filter(isZero));
+
+  console.log(moveZeros([false, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
+}
+
+{
+  const moveZeros = arr => [...arr.filter(el => el !== 0), ...arr.filter(el => el === 0)];
+
+  console.log(moveZeros([false, 2, 0, 1, 0, 1, 0, 3, 0, 1]));
+}
+
+{
+  const moveZeros = arr => {
+    for (let i = arr.length - 1; i >= 0; i--) {
+      if (arr[i] === 0) {
+        arr.splice(i, 1);
+        arr.push(0);
+      }
+    }
+    return arr;
+  };
+}
