670c9b975f09d805e398192e44401083d873464a
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-11-08 21:11:42 +0200

    ...

--- codewars/6-5kyu/Not-very-secure.js
+++ codewars/6-5kyu/Not-very-secure.js
@@ -1,6 +1,5 @@
-const alphanumeric = (string) => {
-  return /^[a-z0-9]+$/i.test(string);
-};
+const alphanumeric = string => /^[a-z0-9]+$/i.test(string);
+
 
 console.log(alphanumeric("Mazinkaiser")); // true
 console.log(alphanumeric("hello world_")); // false
