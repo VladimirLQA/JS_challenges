c0a2d385d63f51813667f5b1a3f88df7e495e84c
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-11-08 21:10:34 +0200

    not very secure 5 kyu

---
+++ codewars/6-5kyu/Not-very-secure.js
@@ -0,0 +1,8 @@
+const alphanumeric = (string) => {
+  return /^[a-z0-9]+$/i.test(string);
+};
+
+console.log(alphanumeric("Mazinkaiser")); // true
+console.log(alphanumeric("hello world_")); // false
+console.log(alphanumeric("PassW0rd")); // true
+console.log(alphanumeric("     ")); // false
