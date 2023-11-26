b8b5f03e38f5337621c009055900a077f33e1d67
lazurenkov@0259 <mr.kfpehtyrj@mail.ru>
2023-11-27 00:25:33 +0200

    ...

---
+++ interview_prep/get_privileges.js
@@ -0,0 +1,16 @@
+const testData = {
+    read: ['fafa@gmail.com', '123@gmail.com', 'wwwwww@gmail.com'],
+    write: ['object@gmail.com', 'array@gmail.com', '123@gmail.com']
+};
+
+const getPrivileges = (data, email) => {
+    const privileges = [];
+    for(const key in data) {
+        if(data[key].find((emailWithPrivileges) => 
+        emailWithPrivileges === email)) privileges.push(key);
+        // if(data[key].includes(email)) privileges.push(key);
+    }
+    return privileges;
+};
+
+console.log(getPrivileges(testData, '123@gmail.com'));
