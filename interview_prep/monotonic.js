d9ee22d3e334ab34a4cf7dd7df2ebad8a907ea9a
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-11-28 20:27:07 +0200

    ...

--- interview_prep/monotonic.js
+++ interview_prep/monotonic.js
@@ -5,20 +5,16 @@
         let isIncreasing = true;
         let isDecreasing = true;
 
-        for (let i = 1; i < array.length - 1; i++) {
-
-            if (array[i] > array[i + 1]) {
-                isDecreasing = false;
-            } else if (array[i] < array[i + 1]) {
-                isIncreasing = false;
-            }
+        for (let i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) isIncreasing = false;
+            if (array[i] < array[i + 1]) isDecreasing = false;
         }
-
+        
         return isIncreasing || isDecreasing;
     }
 
     const isMonotonic = [1, 2, 2, 3];
-    const notMonotonic = [1, 2, 3, 4];
+    const notMonotonic = [ 1, 2, 4, 3];
 
     console.log(monotonic(isMonotonic));
     console.log(monotonic(notMonotonic)); 
