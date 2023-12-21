415295e740aba8ce8bb7be8e5cd1e1d642321fcf
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2023-12-21 23:09:30 +0200

    two solutions for 7kyu

---
+++ codewars/7kyu/descending-order.js
@@ -0,0 +1,24 @@
+/**
+ * Examples:
+ * Input: 42145 Output: 54421
+ * Input: 145263 Output: 654321
+ * Input: 123456789 Output: 987654321
+ */
+
+{
+  const descendingOrder = (num) => {
+    if (typeof num !== 'number' || num < 0) return `Input value should be integer and greater or equal than '0'`;
+
+    return +num
+      .toString()
+      .split('')
+      .sort((num1, num2) => num2 - num1)
+      .join('');
+  };
+
+  console.log(descendingOrder(0)); // 0
+  console.log(descendingOrder(1)); // 1
+  console.log(descendingOrder(111)); // 111
+  console.log(descendingOrder(15)); // 51
+  console.log(descendingOrder(1021)); // 2110
+}
