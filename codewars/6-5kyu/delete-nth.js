a4a39d73a6300195062ce7a6bb78688e21fdaf80
Volodymyr Lazurenko <vlqa@Volodymyrs-MacBook-Pro.local>
2023-12-24 22:22:07 +0200

    ...

--- codewars/6-5kyu/delete-nth.js
+++ codewars/6-5kyu/delete-nth.js
@@ -4,6 +4,7 @@
  * you take [1,2,3,1,2], drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times,
  * and then take 3, which leads to [1,2,3,1,2,3]. With list [20,37,20,21] and number 1,
  * the result would be [20,37,21].
+ * .
  */
 
 {
