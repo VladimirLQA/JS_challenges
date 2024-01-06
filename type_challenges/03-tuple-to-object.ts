70cd2631722415e474e664b4584fc54cc9692794
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-01-06 19:36:37 +0200

    ...

--- type_challenges/03-tuple-to-object.ts
+++ type_challenges/03-tuple-to-object.ts
@@ -2,6 +2,9 @@ type TupleToObject<TTuple extends readonly PropertyKey[]> = {
   [TIndex in TTuple[number]]: TIndex;
 };
 
+type TupleIndex = (typeof tuple)[number];
+//    ^?
+
 const tuple = ['tesla', 'model 2', 'model X', 'model Y'] as const;
 
 type TeslaModels = TupleToObject<typeof tuple>;
