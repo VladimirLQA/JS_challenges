f5c3a0143603e8298403acabb4c2e3e880ad968c
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-02-10 19:37:42 +0200

    code style refactoring

--- how_programming_works/refactoring/functional-good.js
+++ how_programming_works/refactoring/functional-good.js
@@ -3,24 +3,25 @@ const path = require('path');
 
 const filePath = path.resolve(__dirname, 'cities.csv');
 
-const compose = (...funcs) => (x) => funcs.reduce((x, fn) => fn(x), x );
-const proportion = (max, val) => Math.round(val * 100 / max);
+const compose =
+  (...funcs) =>
+  (x) =>
+    funcs.reduce((x, fn) => fn(x), x);
+const proportion = (max, val) => Math.round((val * 100) / max);
 
 const cellPad = (i, str, width) => (i ? str.padStart(width) : str.padEnd(width));
 const cellWIdth = (i) => [18, 10, 8, 8, 18, 6][i];
 
 const renderCell = (cell, i) => cellPad(i, cell + '', cellWIdth(i));
 const renderRow = (row) => row.map(renderCell).join('');
-const renderTable = (table) => table.map(renderRow).join('\n')
+const renderTable = (table) => table.map(renderRow).join('\n');
 
 const densityColumn = () => 3;
-const sortByDensity = table => (
-    table.sort((row1, row2) => (row2[densityColumn()] - row1[densityColumn()]))
-);
+const sortByDensity = (table) =>
+  table.sort((row1, row2) => row2[densityColumn()] - row1[densityColumn()]);
 
-const calcColumn = (table, max) => table.map(
-    (row) => (row.push(proportion(max, row[densityColumn()])), row)
-);
+const calcColumn = (table, max) =>
+  table.map((row) => (row.push(proportion(max, row[densityColumn()])), row));
 
 const calcProportion = (table) => calcColumn(table, table[0][densityColumn()]);
 
@@ -32,4 +33,3 @@ const getDataset = compose(readFile, toLines, parseTable);
 const main = compose(getDataset, sortByDensity, calcProportion, renderTable);
 
 console.log(main(filePath));
-
--- how_programming_works/refactoring/funtional-alternative.js
+++ how_programming_works/refactoring/funtional-alternative.js
@@ -3,30 +3,33 @@ const path = require('path');
 
 const filePath = path.resolve(__dirname, 'cities.csv');
 
-const proportion = (max, val) => Math.round(val * 100 / max);
-const compose = (...funcs) => (x) => funcs.reduce((x, f) => f(x), x);
+const proportion = (max, val) => Math.round((val * 100) / max);
+const compose =
+  (...funcs) =>
+  (x) =>
+    funcs.reduce((x, f) => f(x), x);
 
 const densityCol = 3;
 const cellWidth = [18, 10, 8, 8, 18, 6];
 
-const getDataSet = (file) => fs.readFileSync(file, 'utf8')
+const getDataSet = (file) =>
+  fs
+    .readFileSync(file, 'utf8')
     .split('\n')
     .filter((str, i) => i && str)
     .map((line) => line.split(','));
 
-const calculateProportion = (table) => 
-    ( table.sort((row1, row2) => row2[densityCol] - row1[densityCol]),
-        table.map((row) => 
-        (row.push(proportion(table[0][densityCol], row[densityCol])), row))
-    );
+const calculateProportion = (table) => (
+  table.sort((row1, row2) => row2[densityCol] - row1[densityCol]),
+  table.map((row) => (row.push(proportion(table[0][densityCol], row[densityCol])), row))
+);
 
-const renderTable = (table) => 
-    table.map((row) => row
-        .map((cell, i) => 
-            cell.toString()[(i ? 'padStart' : 'padEnd')](cellWidth[i]))
-        .join(''))
-      .join('\n');
-
+const renderTable = (table) =>
+  table
+    .map((row) =>
+      row.map((cell, i) => cell.toString()[i ? 'padStart' : 'padEnd'](cellWidth[i])).join(''),
+    )
+    .join('\n');
 
 const main = compose(getDataSet, calculateProportion, renderTable);
 
