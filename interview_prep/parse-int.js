b4306d04236183989dd0ed5ddfc48be9aace0bf3
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-03-07 23:13:20 +0200

    moved palindrome file to interview_prep; added parse-int implementation;

---
+++ interview_prep/parse-int.js
@@ -0,0 +1,50 @@
+{
+  const parseToInt = (value) => {
+    if (typeof value !== 'string') return `TypeError: Input must be a string`;
+
+    const strToIntObj = {
+      0: 0,
+      1: 1,
+      2: 2,
+      3: 3,
+      4: 4,
+      5: 5,
+      6: 6,
+      7: 7,
+      8: 8,
+      9: 9,
+    };
+    let result = 0;
+    let sign = 1;
+    let numericPartFound = false;
+
+    if (value[0] === '-') {
+      sign = -1;
+      value = value.slice(1);
+    } else if (value[0] === '+') {
+      value = value.slice(1);
+    }
+
+    for (let i = 0; i < value.length; i++) {
+      let currentValue = value.charAt(i);
+      let num = strToIntObj[currentValue];
+
+      if (num === undefined) {
+        break;
+      }
+      numericPartFound = true;
+      result = result * 10 + num;
+    }
+
+    return numericPartFound ? result * sign : NaN;
+  };
+
+  console.log(parseToInt('10'));
+  console.log(parseToInt('aaa'));
+  console.log(parseToInt('11a1'));
+  console.log(parseToInt('a10'));
+  console.log(parseToInt('-1'));
+  console.log(parseToInt(true));
+  console.log(parseToInt(false));
+  console.log(parseToInt(10));
+}
