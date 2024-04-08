c1ba8ecafc16f8fde30132d3779c0a096eeb90fc
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-04-08 22:46:57 +0300

    added 2 solutions for 7 kyu: count the divisors of a number; is it a letter

---
+++ codewars/7kyu/count-the-divisors-of-a-number.js
@@ -0,0 +1,34 @@
+{
+  const getDivisorsCnt = (n) => {
+    let count = 0;
+
+    for (let i = n; i > 0; i--) {
+      if (n % i === 0) count++;
+      continue;
+    }
+    return count;
+  };
+  console.log(getDivisorsCnt(10)); // 4
+  console.log(getDivisorsCnt(11)); // 2
+  console.log(getDivisorsCnt(54)); // 8
+}
+
+{
+  const getDivisorsCnt = (n) => {
+    if (n === 1) return 1;
+
+    let count = 2; // Initialize count to 2 as 1 and n are always divisors
+
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        if (i !== n / i) count += 2; // Increment count by 2 for each pair of divisors
+        else count++; // Increment count by 1 if the divisor is a perfect square
+      }
+    }
+
+    return count;
+  };
+  console.log(getDivisorsCnt(10)); // 4
+  console.log(getDivisorsCnt(11)); // 2
+  console.log(getDivisorsCnt(54)); // 8
+}
---
+++ codewars/7kyu/is-it-a-letter.js
@@ -0,0 +1,18 @@
+{
+  /**
+   * Write a function, isItLetter or is_it_letter or IsItLetter, which tells us
+   * if a given character is a letter or not.
+   */
+  const isItLetter = (char) => 'qwertyuiopasdfghjklzxcvbnm'.includes(char.toLowerCase());
+
+  console.log(isItLetter('a'));
+  console.log(isItLetter('A'));
+  console.log(isItLetter('1'));
+}
+
+{
+  const isItLetter = (char) => /[a-z]/i.test(char);
+  console.log(isItLetter('a'));
+  console.log(isItLetter('A'));
+  console.log(isItLetter('1'));
+}
