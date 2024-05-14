330664ddf50e7c644f72111f1deb5f7a8eddebd7
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-05-14 21:54:52 +0300

    code style fix

--- gpt_tasks/generics.ts
+++ gpt_tasks/generics.ts
@@ -7,30 +7,34 @@
  * Write test cases to demonstrate the functionality of this higher-order function with different data types.*/
 
 const testArrayNumbers = [1, 5, 6, 9, 11, 15];
-const testArrayStrings = ['123', 'first ', "don't judge book by it's cover", '9'];
+const testArrayStrings = ['123', 'first ',
+  'don\'t judge book by it\'s cover', '9'];
 
 type FilterCallBack<T> = (element: T, index: number, array: T[]) => boolean;
 type MapCallBack<T, U> = (element: T, index: number, array: T[]) => U;
 
-const mapper: MapCallBack<string | number, any> = (value, index, array) => {
-  if (typeof value === 'string') {
-    return value.toUpperCase();
-  } else {
-    return value ** 3;
-  }
-};
+const mapper: MapCallBack<string | number, any> =
+  (value, index, array) => {
+    if (typeof value === 'string') {
+      return value.toUpperCase();
+    } else {
+      return value ** 3;
+    }
+  };
 
-const filterFunction: FilterCallBack<string | number> = (value, index, array) => {
-  if (typeof value === 'string') {
-    return value.length > 5;
-  } else {
-    return value > 729;
-  }
-};
+const filterFunction: FilterCallBack<string | number> =
+  (value, index, array) => {
+    if (typeof value === 'string') {
+      return value.length > 5;
+    } else {
+      return value > 729;
+    }
+  };
 
-function mapWithFilter<T, U>(array: T[], mapFunc: MapCallBack<T, U>, filterFunc: FilterCallBack<U>): U[] {
-  return array.map(mapFunc).filter(filterFunc);
-}
+const mapWithFilter =
+  <T, U>(array: T[], mapFunc: MapCallBack<T, U>, filterFunc: FilterCallBack<U>):
+    U[] => array.map(mapFunc).filter(filterFunc);
+
 
 console.log(mapWithFilter(testArrayNumbers, mapper, filterFunction));
 console.log(mapWithFilter(testArrayStrings, mapper, filterFunction));
