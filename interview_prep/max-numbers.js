2c054e0c21aaa4421c05dc4b95823513d91fc3a1
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-06-16 21:47:59 +0300

    2 solutions in interview prep tasks

---
+++ interview_prep/max-numbers.js
@@ -0,0 +1,8 @@
+const getMaxNumbers = (arr) =>
+  arr.map((item) =>
+    +item.toString()
+      .split('')
+      .sort((a, b) => +b - +a)
+      .join(''));
+
+console.log(getMaxNumbers([123, 902])); // [321, 920]
