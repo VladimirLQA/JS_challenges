a0470bdb78239c7c9111f4a53721023408752ab8
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-06-27 22:37:34 +0300

    8 kyu solutions

---
+++ codewars/8kyu/alternating-case.js
@@ -0,0 +1,29 @@
+/*
+ Each lowercase letter becomes uppercase and each uppercase letter becomes lowercase.
+*/
+
+{
+  String.prototype.toAlternatingCase = function() {
+    return this.split('').map((char) =>
+      char.toUpperCase() === char
+        ? char.toLowerCase()
+        : char.toUpperCase()).join('');
+  };
+
+  console.log('hello'.toAlternatingCase());
+  console.log('HELLO'.toAlternatingCase());
+  console.log('hELlo .123'.toAlternatingCase());
+}
+
+{
+  const isLowerCase = (char) => char === char.toLowerCase();
+  const changeCase = (char) =>
+    isLowerCase(char) ? char.toUpperCase() : char.toLowerCase();
+
+  String.prototype.toAlternatingCase = function() {
+    return this.split('').map(changeCase).join('');
+  };
+  console.log('hello'.toAlternatingCase());
+  console.log('HELLO'.toAlternatingCase());
+  console.log('hELlo .123'.toAlternatingCase());
+}
---
+++ codewars/8kyu/welcome.js
@@ -0,0 +1,34 @@
+const solute = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso',
+};
+{
+  const greet = (lang) => solute[lang] ? solute[lang] : 'Welcome';
+
+  console.log(greet('english'));
+  console.log(greet('dutch'));
+  console.log(greet('IP_ADDRESS_INVALID'));
+}
+
+{
+  const greet = (lang) => solute[lang] || solute['english'];
+
+  console.log(greet('english'));
+  console.log(greet('dutch'));
+  console.log(greet('IP_ADDRESS_INVALID'));
+}
