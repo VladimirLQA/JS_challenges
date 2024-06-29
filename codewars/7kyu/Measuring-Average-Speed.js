b66a8f5c47c925521c576634a2a1dbc9cbf591c8
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-06-29 18:42:43 +0300

    alternative solution + description added

--- codewars/7kyu/Measuring-Average-Speed.js
+++ codewars/7kyu/Measuring-Average-Speed.js
@@ -24,8 +24,8 @@ console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
 {
   const calculateSpeed = (distance, time) => {
     const mS = 2.23694;
-    let d = parseInt(distance, 10) * (distance.endsWith('km') ? 1000 : 1);
-    let t = parseInt(time, 10) * (time.endsWith('min') ? 60 : 1);
+    const d = parseInt(distance, 10) * (distance.endsWith('km') ? 1000 : 1);
+    const t = parseInt(time, 10) * (time.endsWith('min') ? 60 : 1);
 
     return `${((d / t) * mS).toFixed()}mph`;
   };
@@ -39,12 +39,12 @@ console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
 console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
 
 {
-  function calculateSpeed(distance, time) {
-    let d = parseInt(distance, 10) * (distance.match(/km$/) ? 1000 : 1);
-    let t = parseInt(time, 10) * (time.match(/min$/) ? 60 : 1);
+  const calculateSpeed = (distance, time) => {
+    const d = parseInt(distance, 10) * (distance.match(/km$/) ? 1000 : 1);
+    const t = parseInt(time, 10) * (time.match(/min$/) ? 60 : 1);
 
     return Math.round((d / t) * 2.23694) + 'mph';
-  }
+  };
 
   console.log(calculateSpeed('100m', '10s')); // 22mph
   console.log(calculateSpeed('3km', '5min')); // 22mph
--- codewars/7kyu/find-middle-of-the-product.js
+++ codewars/7kyu/find-middle-of-the-product.js
@@ -1,3 +1,15 @@
+/*
+ Given a string of characters, I want the function findMiddle()/find_middle()
+ to return the middle number in the product of each digit in the string.
+ Example: 's7d8jd9' -> 7, 8, 9 -> 7*8*9=504, thus 0 should be returned as an integer.
+
+ Not all strings will contain digits. In this case and the case for any non-strings, return -1.
+ If the product has an even number of digits, return the middle two digits
+
+ Example: 1563 -> 56
+ NOTE: Remove leading zeros if product is even and
+ the first digit of the two is a zero. Example 2016 -> 1
+*/
 {
   const findMiddleElements = (arr) =>
     arr.length % 2 === 0
@@ -15,6 +27,19 @@
       findMiddleElements([...product.toString()].map(Number));
 
     return +middleElements.join('');
+  };
+
+  console.log(findMiddle('58jd9fgh/fgh6s.,sdf'));
+}
+
+{
+  const findMiddle = (str) => {
+    if (typeof str !== 'string' || !/\d/.test(str)) return -1;
+    const product = Array.from(str.replace(/\D/g, ''))
+      .reduce((x, y) => x * y).toString();
+    return +product
+      .slice(Math.ceil(product.length / 2 - 1),
+        Math.floor(product.length / 2 + 1));
   };
 
   console.log(findMiddle('58jd9fgh/fgh6s.,sdf'));
