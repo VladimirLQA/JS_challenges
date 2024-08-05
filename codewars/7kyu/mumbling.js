255136eb1654aca1905e1931ba56789f2de390f9
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-08-05 19:17:02 +0300

    7k - 2 solutions

--- codewars/7kyu/mumbling.js
+++ codewars/7kyu/mumbling.js
@@ -4,7 +4,7 @@
   accum("abcd") -> "A-Bb-Ccc-Dddd"
   accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
   accum("cwAt") -> "C-Ww-Aaa-Tttt"
- The parameter of accum is a string which includes only letters from a..z and A..Z.
+ The parameter of accum is a string which includes only letters from a...z and A...Z.
 */
 {
   const toUpperCaseFirstChar = (str) =>
@@ -21,6 +21,16 @@
         ],
         [],
       )
+      .join('-');
+
+  console.log(accum('ZpglnRxqenU'));
+  console.log(accum('NyffsGeyylB'));
+  console.log(accum('MjtkuBovqrU'));
+}
+
+{
+  const accum = (s) =>
+    s.split('').map((c, i) => c.toUpperCase() + c.toLowerCase().repeat(i))
       .join('-');
 
   console.log(accum('ZpglnRxqenU'));
