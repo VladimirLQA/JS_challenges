bab28f73758c3e83814f0752b6bdab5e1b442d0e
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-08-13 12:14:06 +0300

    ...

--- string_methods/endsWith.js
+++ string_methods/endsWith.js
@@ -1,4 +1,40 @@
 {
+  const customEndsWith = (str, searchValue, endPosition = str.length) => {
+    let strToCompare = '';
+    let searchIndex = searchValue.length - 1;
+
+    for (let i = endPosition - 1; i >= 0 && searchIndex >= 0; i--) {
+      strToCompare = str[i] + strToCompare;
+      searchIndex--;
+    }
+    return strToCompare === searchValue;
+  };
+
+  console.log(customEndsWith('To be, or not to be,' +
+    'that is the question.', 'question.'));
+  console.log(customEndsWith('To be, or not to be,' +
+    'that is the question.', 'to be', 19));
+}
+{
+  const customEndsWith = (str, searchValue, endPosition = str.length) => {
+    let searchIndex = searchValue.length - 1;
+
+    for (let i = endPosition - 1; i >= 0 && searchIndex >= 0; i--) {
+      if (str[i] !== searchValue[searchIndex]) {
+        return false;
+      }
+      searchIndex--;
+    }
+    return searchIndex === -1;
+  };
+
+  console.log(customEndsWith('To be, or not to be,' +
+    'that is the question.', 'question.'));
+  console.log(customEndsWith('To be, or not to be,' +
+    'that is the question.', 'to be', 19));
+}
+
+{
   const customEndsWith = (str, searchValue, endPosition) => {
     let slicedStr = '';
     if (endPosition) {
