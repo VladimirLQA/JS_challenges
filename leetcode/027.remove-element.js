6ad91d23c76f225c9ee4363ab36641c61425003c
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2024-08-23 20:43:08 +0300

    027 - remove element

---
+++ leetcode/027.remove-element.js
@@ -0,0 +1,19 @@
+{
+  const removeElement = (nums, val) => nums.filter((n) => n !== val).length;
+
+  console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)); // 5
+}
+{
+  const removeElement = (nums, val) => {
+    let k = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] !== val) {
+        nums[k] = nums[i];
+        k++;
+      }
+    }
+    return k;
+  };
+  console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)); // 5
+}
