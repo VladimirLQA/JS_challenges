378afcb34ea039e33491531597d8ee8e32e06a4f
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2025-01-02 00:04:36 +0200

    8k - 1

---
+++ codewars/8kyu/smallest-unused-id.js
@@ -0,0 +1,30 @@
+/*
+
+*/
+
+{
+  const nextId = (ids) => {
+    let result;
+    const sortedIDs = ids.sort((a, b) => a - b);
+    for (let i = 0; i < sortedIDs.length; i++) {
+      if (!sortedIDs.includes(i)) return i;
+      result = i + 1;
+    }
+    return result;
+  };
+
+  console.log(nextId([0, 1, 2, 3, 5])); // 4
+  console.log(nextId([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 11
+}
+{
+  const nextId = (ids) => {
+    const unique = new Set(ids);
+    let i = 0;
+    while (true) {
+      if (!unique.has(i)) return i;
+      i++;
+    }
+  };
+  console.log(nextId([0, 1, 2, 3, 5])); // 4
+  console.log(nextId([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])); // 11
+}
