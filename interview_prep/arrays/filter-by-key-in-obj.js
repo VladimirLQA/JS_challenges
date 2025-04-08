61778f759a3b8b48bb39c58dd687b94564fcec67
VolodymyrLQA <vovaomgxdlazurenko@gmail.com>
2025-04-08 18:33:21 +0300

    refactoring

--- interview_prep/arrays/filter-by-key-in-obj.js
+++ interview_prep/arrays/filter-by-key-in-obj.js
@@ -5,25 +5,32 @@ const people = [
   { name: 'Charlie', age: 30 },
   {},
 ];
-people.length = 10;
 
-const moreThan = (v) => v > 25;
-const personName = (name) => name === 'Alice';
+const numberMoreThan25 = (v) => v > 25;
+const personNameEqualsToAlice = (name) => name === 'Alice';
 
 const filter = (arr, key, predicate) =>
   arr.filter((obj) => key in obj && predicate(obj[key]));
 
-console.log(filter(people, 'age', moreThan));
-console.log(filter(people, 'name', personName));
+console.log(filter(people, 'age', numberMoreThan25));
+console.log(filter(people, 'name', personNameEqualsToAlice));
 
 const mapper = {
   '>': (a, b) => a > b,
 };
 
-const filter1 = (arr, key, operator, value) => {
-  if (!(operator in mapper)) throw new Error('Operator was not found');
+const keysOfPersonObject = ['age', 'name'];
+
+const filterWithMapping = (arr, key, operator, value) => {
+  if (!keysOfPersonObject.includes(key)) {
+    throw new Error(`Key ${key} does not exist in person object`);
+  }
+  if (!(operator in mapper)) {
+    throw new Error('Comparision operator was not found');
+  }
   const func = mapper[operator];
   return arr.filter((obj) => key in obj && func(obj[key], value));
 };
 
-console.log(filter1(people, 'age', '>', 25));
+console.log(filterWithMapping(people, 'age', '>', 25));
+console.log(filterWithMapping(people, 'isAdmin', '>', 25));
